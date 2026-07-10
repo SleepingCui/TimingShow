929132c1b08ceefd61b6fbe137a2b97fdd5f0712
SleepingCui <130898297+SleepingCui@users.noreply.github.com>
2026-07-10 17:16:10 +0800

    build

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 msbuild TimingShow.sln /p:Configuration=Release /p:Platform="Any CPU" /v:m
-powershell -NoProfile -Command "Compress-Archive -Path 'TimingShow\bin\Release\TimingShow.dll','TimingShow\bin\Release\Info.json' -DestinationPath 'TimingShow.zip' -Force"
+powershell -NoProfile -Command "Compress-Archive -Path 'TimingShow\bin\Release\TimingShow.dll','TimingShow\bin\Release\Info.json','TimingShow\bin\Release\lang.json' -DestinationPath 'TimingShow.zip' -Force"
 echo OK
 pause
